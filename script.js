9a6f704f3fca48352c691bd3518e4705f254bb58
jesswinw <jesswinw@gmail.com>
2020-05-26 18:43:02 +0530

    new reset feature added

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const watch = document.querySelector(".timer");
 const btn = document.querySelector(".start");
-
+const icon = document.querySelector(".fas");
+let timerId;
 //other
 watch.innerText = "25:00";
 
 // Listeners
 btn.addEventListener("click", () => {
+  icon.classList[1] === "fa-chevron-right" ? startTime() : endTime();
+});
+
+//functions
+const startTime = () => {
   let time = 25 * 60;
-  btn.classList.toggle("disabled");
-  btn.disabled = true;
-  const timer = setInterval(() => {
-    if (time <= 0) {
-      btn.classList.toggle("disabled");
-      btn.disabled = false;
-      clearInterval(timer);
-      watch.innerText = "25:00";
-      return 0;
-    }
+  icon.classList.toggle("fa-chevron-right");
+  icon.classList.toggle("fa-stop");
+  timerId = setInterval(() => {
     time--;
     let hour = Math.floor(time / 60);
     hour = hour >= 10 ? hour : "0" + hour;
@@ -26,5 +25,16 @@ btn.addEventListener("click", () => {
     console.log(`${hour}:${sec}`);
 
     watch.innerText = `${hour}:${sec}`;
+    if (time <= 0) {
+      endTime();
+    }
   }, 1000);
-});
+};
+
+const endTime = () => {
+  console.log("End time()");
+  clearInterval(timerId);
+  watch.innerText = "25:00";
+  icon.classList.toggle("fa-chevron-right");
+  icon.classList.toggle("fa-stop");
+};
