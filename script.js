bb3c2607267a19b1837d6ebf7677d819161d0ac0
jesswinw <jesswinw@gmail.com>
2020-06-18 15:30:00 +0530

    added sound ath the end

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-const watch = document.querySelector(".timer");
-const btn = document.querySelector(".start");
-const icon = document.querySelector(".fas");
+const watch = document.querySelector('.timer');
+const btn = document.querySelector('.start');
+const icon = document.querySelector('.fas');
 let timerId;
 //other
-watch.innerText = "25:00";
-
+watch.innerText = '25:00';
+//alarm sound
+const alarm = new Audio('./sounds/alarm.mp3');
 // Listeners
-btn.addEventListener("click", () => {
-  icon.classList[1] === "fa-chevron-right" ? startTime() : endTime();
+btn.addEventListener('click', () => {
+	icon.classList[1] === 'fa-chevron-right' ? startTime() : resetTime();
 });
 
 //functions
 const startTime = () => {
-  let time = 25 * 60;
-  icon.classList.toggle("fa-chevron-right");
-  icon.classList.toggle("fa-stop");
-  timerId = setInterval(() => {
-    time--;
-    let hour = Math.floor(time / 60);
-    hour = hour >= 10 ? hour : "0" + hour;
-    let sec = Math.floor(time % 60);
-    sec = sec >= 10 ? sec : "0" + sec;
+	alarm.pause();
+	let time = 25 * 60;
+	icon.classList.toggle('fa-chevron-right');
+	icon.classList.toggle('fa-stop');
+	timerId = setInterval(() => {
+		time--;
+		let hour = Math.floor(time / 60);
+		hour = hour >= 10 ? hour : '0' + hour;
+		let sec = Math.floor(time % 60);
+		sec = sec >= 10 ? sec : '0' + sec;
 
-    console.log(`${hour}:${sec}`);
+		console.log(`${hour}:${sec}`);
 
-    watch.innerText = `${hour}:${sec}`;
-    if (time <= 0) {
-      endTime();
-    }
-  }, 1000);
+		watch.innerText = `${hour}:${sec}`;
+		if (time <= 0) {
+			endTime();
+		}
+	}, 1000);
 };
 
 const endTime = () => {
-  console.log("End time()");
-  clearInterval(timerId);
-  watch.innerText = "25:00";
-  icon.classList.toggle("fa-chevron-right");
-  icon.classList.toggle("fa-stop");
+	alarm.play();
+	resetTime();
+};
+const resetTime = () => {
+	console.log('End time()');
+	clearInterval(timerId);
+	watch.innerText = '25:00';
+	icon.classList.toggle('fa-chevron-right');
+	icon.classList.toggle('fa-stop');
 };
